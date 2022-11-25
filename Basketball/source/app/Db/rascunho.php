05434932377e027c0b2501e7ec3994b6bab6e3a0
osaghost <106832403+osaghost@users.noreply.github.com>
2022-11-25 09:41:55 -0300

    Delete Basketball directory

--- Basketball/source/app/Db/rascunho.php
+++
@@ -1,9 +0,0 @@
-$obDatabase = new Database('teams');
-        $this->id = $obDatabase->insert([
-                                        'team_name' => $this->name,
-                                        'contact' => $this->contact,
-                                        'modality' => $this->modality,
-                                        'gender' => $this->gender,
-                                        'state' => $this->state,
-                                        'city' => $this->city  
-                                    ]);
--- Basketball/source/boot/Config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('HOST', '127.0.0.1');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DB', 'bd-sql');
